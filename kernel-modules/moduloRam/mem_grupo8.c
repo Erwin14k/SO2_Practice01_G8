8d2e931b7e0f402c75fecd602a1f1ba9747b9633
Erwin_14k <82339728+Erwin14k@users.noreply.github.com>
2023-06-08 14:15:57 -0600

    memomy module source code commented

--- kernel-modules/moduloRam/mem_grupo8.c
+++ kernel-modules/moduloRam/mem_grupo8.c
@@ -1,58 +1,66 @@
 #include <linux/mm.h> 
-//Header para la ram
+//Header to use the memory info
 #include <linux/module.h>
-// para usar KERN_INFO
+// To use kern info
 #include <linux/kernel.h>
-//Header para los macros module_init y module_exit
+//Header to use the init and exit macros
 #include <linux/init.h>
-//Header necesario porque se usara proc_fs
+//Header neccesary to use proc_fs
 #include <linux/proc_fs.h>
 /* for copy_from_user */
 #include <asm/uaccess.h>	
-/* Header para usar la lib seq_file y manejar el archivo en /proc*/
+/* Header to use the seq_file library and manage the file on  /proc*/
 #include <linux/seq_file.h>
 
 
-
+// Module info
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Modulo RAM, Laboratorio Sistemas Operativos 2");
 MODULE_AUTHOR("Sergie Daniel Arizandieta Yol");
 
-
+// Function to write the file
 static int escribir_archivo(struct seq_file *archivo, void *v)
 {
+    // struct sysinfo
     struct sysinfo info;
     si_meminfo(&info);
 
+    //Init the file with a {
     seq_printf(archivo, "{\n");
+    //The total ram on the system
     seq_printf(archivo, "\"totalram\":%lu,\n", info.totalram * info.mem_unit / 1024 / 1024);
+    //The free ram on the system
     seq_printf(archivo, "\"ramlibre\":%lu,\n", info.freeram * info.mem_unit / 1024 / 1024);
+    //The occupied ram on the system
     seq_printf(archivo, "\"ramocupada\":%lu\n", (info.totalram -  info.freeram )* info.mem_unit / 1024 / 1024);
+    //Closing the file with a }
     seq_printf(archivo, "}\n");
 
     return 0;
 }
 
-//Funcion que se ejecuta cuando se le hace un cat al modulo.
+//Funcion executed every time with a cat command 
 static int al_abrir(struct inode *inode, struct file *file)
 {
     return single_open(file, escribir_archivo, NULL);
 }
 
-// Si el Kernel es 5.6 o mayor
+// If the kernel is 5.6 or higher
 static struct proc_ops operaciones =
 {
     .proc_open = al_abrir,
     .proc_read = seq_read
 };
 
+
+// Function to display the message when the module is created with insmod
 static int _insert(void)
 {
     proc_create("mem_grupo8", 0, NULL, &operaciones);
     printk(KERN_INFO "Hola mundo, somos el grupo 8 y este es el monitor de memoria\n");
     return 0;
 }
-
+// Function to display the message when the module is deleted with rmmod
 static void _remove(void)
 {
     remove_proc_entry("mem_grupo8", NULL);
